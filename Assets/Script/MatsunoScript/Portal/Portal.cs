5b4d2fb8c99d37d4e1703d2dc008a5642c002f81
Matsuno Masayuki <riana352@gmail.com>
2022-03-28 05:09:47 +0900

    ポータルの破壊(ScriptableObjectで管理)

---
+++ Assets/Script/MatsunoScript/Portal/Portal.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    public PortalData PortalData;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Debug.Log(PortalData.Hp);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "Player")
+        {
+            PortalData.Hp--;
+
+            if (PortalData.Hp <= 0)
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
