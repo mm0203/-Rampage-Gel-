d2f65eada7be6604d61b693afd0e28d3b8accd2c
DESKTOP-ET14MOC\hukiki <syun3430215@outlook.com>
2022-03-28 17:23:21 +0900

    SetDestinationエラー改善

--- Assets/08_Script/OguraScript/Boss01/Boss01.cs
+++ Assets/08_Script/OguraScript/Boss01/Boss01.cs
@@ -104,9 +104,10 @@ public class Boss01 : MonoBehaviour
     //-------------------------
     void Update()
     {
-        Death();
         Move();
         Attack();
+        Death();
+
     }
 
     //----------------------------
