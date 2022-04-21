74b565307a2388a0cf7300fa8919c9790289c915
KoshiroTakeo <80869009+KoshiroTakeo@users.noreply.github.com>
2022-04-21 12:33:41 +0900

    LVupによるアイテム選択、強化実装

--- Assets/08_Script/Item/ItemManager.cs
+++ Assets/08_Script/Item/ItemManager.cs
@@ -5,11 +5,13 @@
 //
 // 2022/03/09 author：畠山大輝 実装開始(種類,生成の仮実装)
 // 2022/03/15                  アイテム追加分の実装(分岐を増やすだけ)
+// 2022/04/21 author：竹尾晃史郎 nItemCount修正
 //
 //======================================================================
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
@@ -47,6 +49,9 @@ public class ItemManager : MonoBehaviour
     public List<int> CountList;
     public int nStatus;
 
+    // アイテム画像データ（竹）
+    public ItemImage itemImage;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -55,15 +60,17 @@ public class ItemManager : MonoBehaviour
             CountList.Add(0);
         }
 
-        // 仮
-        nStatus = Random.Range(0, (int)eItem.eMax - 1);
-        CreateItem();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            nStatus = Random.Range(0, (int)eItem.eMax - 1);
+            nItemCount(nStatus);
+        }
     }
 
     // 新規オブジェクトの生成関数
@@ -77,11 +84,27 @@ public class ItemManager : MonoBehaviour
     }
 
     // オブジェクトの取得個数をカウント
-    public int nItemCount()
+    public int nItemCount(int itemID)
     {
-        CountList[nStatus]++;
-        Debug.Log(nStatus + "番アイテム：" + CountList[nStatus] + "個");
+        CountList[itemID]++;
+        Debug.Log(itemID + "番アイテム：" + CountList[itemID] + "個");
 
         return CountList[nStatus];
+    }
+
+    // アイテム抽選（竹尾）
+    public int nItemGacha()
+    {
+        int nItemID = 0;
+        nItemID = Random.Range(0, (int)eItem.eMax - 1);
+        return nItemID;
+    }
+
+    // アイテム画像渡し（竹）
+    public Sprite setItemIcon(int itemID)
+    {
+        Sprite image;
+        image =  itemImage.ItemImageList[itemID];
+        return image;
     }
 }
--- Assets/08_Script/System/Pause/Pause.cs
+++ Assets/08_Script/System/Pause/Pause.cs
@@ -6,10 +6,12 @@
 // 2022/03/23 author：奥田達磨 ポーズ機能実装
 // 2022/03/25 author：奥田達磨 ポーズメニュー・レベルアップメニュー機能追加 
 // 2022/04/25 author：奥田達磨 ポーズバグ修正 
+// 2022/04/21 author：竹尾晃史郎 アイテム決定機能追加
 //
 //======================================================================
 using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class Pause : MonoBehaviour
 {
@@ -24,6 +26,12 @@ public class Pause : MonoBehaviour
     [SerializeField] List<GameObject> gPauseMenuChoice;
     [SerializeField] List<GameObject> gLevelUpMenuChoice;
 
+    // 4/21 追加（竹）
+    [SerializeField] ItemManager itemManager;
+    public Image SelectItemIcon_L, SelectItemIcon_C, SelectItemIcon_R;
+    int setItem_L = 0, setItem_C = 0, setItem_R = 0;
+    bool bLotteryComp = false;
+
 
     void Start()
     {
@@ -32,6 +40,9 @@ public class Pause : MonoBehaviour
         bPause = false;
         bLevelUpPause = false;
         bResume = true;
+
+        // 4/21 追加
+        itemManager = GameObject.FindWithTag("ItemManager").GetComponent<ItemManager>();
         
     }
     void Update()
@@ -76,6 +87,8 @@ public class Pause : MonoBehaviour
         //レベルアップ時のポーズメニュー
         if(bLevelUpPause)
         {
+            // 4/21 追加
+            //itemManager
             levelUpPause(bLevelUpPause);
         }
         else
@@ -177,35 +190,86 @@ public class Pause : MonoBehaviour
     public void levelUpPause(bool b)
     {
         gLevelUpPause.SetActive(b);
-        if (Input.GetKeyDown(KeyCode.RightArrow))
+
+        // LVUPしなくてもずっと呼ばれるためif追加（竹尾）
+        if(b)
         {
-            nMenuFrame++;
-            if (nMenuFrame > 2)
-                nMenuFrame = 0;
+            // アイテム番号割り当て（竹尾）
+            if(bLotteryComp == false)
+            {
+                setItem_L = itemManager.nItemGacha(); // "L" eft
+                setItem_C = itemManager.nItemGacha(); // "C" enter
+                setItem_R = itemManager.nItemGacha(); // "R" ight
+
+                SelectItemIcon_L.sprite = itemManager.setItemIcon(setItem_L);
+                SelectItemIcon_C.sprite = itemManager.setItemIcon(setItem_C);
+                SelectItemIcon_R.sprite = itemManager.setItemIcon(setItem_R);
+
+                bLotteryComp = true;
+            }
+            
+
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                nMenuFrame++;
+                if (nMenuFrame > 2)
+                    nMenuFrame = 0;
+            }
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                nMenuFrame--;
+                if (nMenuFrame < 0)
+                    nMenuFrame = 2;
+            }
+            if (nMenuFrame == 0)
+            {
+                Debug.Log("左アイテム選択中");
+                
+                gLevelUpMenuChoice[nMenuFrame].SetActive(true);
+                gLevelUpMenuChoice[nMenuFrame + 1].SetActive(false);
+                gLevelUpMenuChoice[nMenuFrame + 2].SetActive(false);
+
+                //決定処理（竹尾）
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    itemManager.nItemCount(setItem_L);
+                    SetbLevelPause(false);
+                    bLotteryComp = false;
+                }
+            }
+            if (nMenuFrame == 1)
+            {
+                Debug.Log("中央アイテム選択中");
+                
+                gLevelUpMenuChoice[nMenuFrame].SetActive(true);
+                gLevelUpMenuChoice[nMenuFrame - 1].SetActive(false);
+                gLevelUpMenuChoice[nMenuFrame + 1].SetActive(false);
+
+                //決定処理（竹尾）
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    itemManager.nItemCount(setItem_C);
+                    SetbLevelPause(false);
+                    bLotteryComp = false;
+                }
+            }
+            if (nMenuFrame == 2)
+            {
+                Debug.Log("右アイテム選択中");
+                
+                gLevelUpMenuChoice[nMenuFrame].SetActive(true);
+                gLevelUpMenuChoice[nMenuFrame - 1].SetActive(false);
+                gLevelUpMenuChoice[nMenuFrame - 2].SetActive(false);
+
+                //決定処理（竹尾）
+                if (Input.GetKeyDown(KeyCode.Return))
+                {
+                    itemManager.nItemCount(setItem_R);
+                    SetbLevelPause(false);
+                    bLotteryComp = false;
+                }
+            }
         }
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
-        {
-            nMenuFrame--;
-            if (nMenuFrame < 0)
-                nMenuFrame = 2;
-        }
-        if (nMenuFrame == 0)
-        {
-            gLevelUpMenuChoice[nMenuFrame].SetActive(true);
-            gLevelUpMenuChoice[nMenuFrame + 1].SetActive(false);
-            gLevelUpMenuChoice[nMenuFrame + 2].SetActive(false);
-        }
-        if (nMenuFrame == 1)
-        {
-            gLevelUpMenuChoice[nMenuFrame].SetActive(true);
-            gLevelUpMenuChoice[nMenuFrame - 1].SetActive(false);
-            gLevelUpMenuChoice[nMenuFrame + 1].SetActive(false);
-        }
-        if(nMenuFrame == 2)
-        {
-            gLevelUpMenuChoice[nMenuFrame].SetActive(true);
-            gLevelUpMenuChoice[nMenuFrame - 1].SetActive(false);
-            gLevelUpMenuChoice[nMenuFrame - 2].SetActive(false);
-        }
+       
     }
 }
