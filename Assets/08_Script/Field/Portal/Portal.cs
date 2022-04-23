1e06dcbc1e3ac99883bbb3c5efe7b8c70394ea8e
KoshiroTakeo <80869009+KoshiroTakeo@users.noreply.github.com>
2022-04-23 11:43:47 +0900

    シーン遷移管理しやすくした

--- Assets/08_Script/Field/Portal/Portal.cs
+++ Assets/08_Script/Field/Portal/Portal.cs
@@ -4,6 +4,7 @@
 // 開発履歴
 //
 // 2022/04/01 author：松野将之 ポータルのシーン遷移
+// 2022/04/23 author：竹尾晃史郎　シーン追加が面倒なためSceneData追加
 //
 //======================================================================
 using System.Collections;
@@ -16,6 +17,15 @@ public class Portal : MonoBehaviour
     // 次のシーン
     public SceneObject m_nextScene;
 
+    // 0423 シーン管理
+    public SceneData SceneData;
+    [SerializeField] string sNowScene = "現在のシーン";
+
+    private void Start()
+    {
+        DecideNextScene();
+    }
+
     // プレイヤーと接触でシーン遷移
     private void OnCollisionEnter(Collision collision)
     {
@@ -25,4 +35,23 @@ public class Portal : MonoBehaviour
             FadeManager.Instance.LoadScene(m_nextScene, 1.0f);
         }
     }
+
+    // 0423 シーン決定 *****************************************
+    void DecideNextScene()
+    {
+        m_nextScene = null;
+        sNowScene = SceneManager.GetActiveScene().name;
+
+        for(int i = 0; i <= SceneData.GameScene.Count; i++)
+        {
+            Debug.Log(SceneData.GameScene[i].m_SceneName);
+            if(sNowScene == SceneData.GameScene[i].m_SceneName)
+            {
+                m_nextScene = SceneData.GameScene[i + 1];
+                break;
+            }
+        }
+        if (m_nextScene == null) Debug.LogError("次に遷移するシーンがありません");
+    }
+    //**********************************************************
 }
